89f3339adf9d2bc54540f7319a66451600b644a6
winniefong1021 <winniefong21@gmail.com>
2019-04-30 11:30:07 -0700

    Start on fetchProfileAndWriteToFile

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,14 +10,47 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var promiseFunc = require('./promisification.js');
 
-
-
+fs = Promise.promisifyAll(fs);
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // return fs.readFile(readFilePath, 'utf-8', (err, file) => {
+  // .then( get first line of file  = user ) {
+  // return promiseFunc.getGitHubProfileAsync(user, cb)
+  // }
+  // .then () {
+  // return fs.writeFile(writeFilePath, data, cb)
+  //}
+  // });
+  return fs.readFileAsync(readFilePath, 'utf-8', (err, file) => {
+    console.log(file);
+  });
+  // need to import pluckFirstLineFromFileAsync 
 };
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
+
+// Promise.promisifyAll(db)
+
+// var addNewUserToDatabaseAsync = function(user) {
+//   // The outermost `return` lets us continue the chain
+//   // after an invocation of `addNewUserToDatabaseAsync`
+//   return db.findUserInDatabaseAsync(user)
+//     .then(function(existingUser) {
+//       if (existingUser) {
+//         throw new Error('User already exists!') // Head straight to `catch`. Do not pass Go, do not collect $200
+//       } else {
+//         return user; // Return a synchronous value
+//       }
+//     })
+//     .then(function(newUser) {
+//       return db.hashPasswordAsync(newUser) // Return a promise
+//     })
+//     .then(function(securedUser) {
+//       return db.createAndSaveUserAsync(securedUser) // Return another promise
+//     })
+// }
