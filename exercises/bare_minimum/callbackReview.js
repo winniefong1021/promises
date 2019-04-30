2a5221c86a8ef6f181ffb843eae6f98ce4a5c126
winniefong1021 <winniefong21@gmail.com>
2019-04-29 20:48:19 -0700

    Implement pluckFirstLine

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,22 @@
 
 var fs = require('fs');
 var request = require('request');
+var $ = require('jquery');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      var dataArr = data.split('\n');
+      cb(null, dataArr[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
 };
 
 // Export these functions so we can test them and reuse them in later exercises
