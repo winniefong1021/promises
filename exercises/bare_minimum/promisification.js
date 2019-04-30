a19f353742aedb9ce305f479a3183cb5517a2c27
winniefong1021 <winniefong21@gmail.com>
2019-04-30 11:00:50 -0700

    Finish promisification

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -13,7 +13,7 @@ var getGitHubProfile = function(user, callback) {
   var options = {
     url: 'https://api.github.com/users/' + user,
     headers: { 'User-Agent': 'request' },
-    json: true  // will JSON.parse(body) for us
+    json: true // will JSON.parse(body) for us
   };
 
   request.get(options, function(err, res, body) {
